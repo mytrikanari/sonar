c24d53db305f06a3e64e39150119558957290643
mytrikan <mytrikan@10.4.33.58>
2023-10-12 11:25:54 -0700

    hdsfhwr

--- src/main/java/SonarTest.java
+++ src/main/java/SonarTest.java
@@ -1,12 +1,13 @@
 
 public class SonarTest {
+	public class abcd {
 		public String b;
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			public String a;
 			System.out.println(a);
-			System.out.println(sonar );
+			System.out.println(sonar cloud);
 			
 		}
 
