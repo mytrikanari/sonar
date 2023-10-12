8541150d3ba1844faaa051b5429e0479e580f4d3
mytrikan <mytrikan@10.4.33.58>
2023-10-12 11:25:10 -0700

    hdsfhwr

---
+++ src/main/java/SonarTest.java
@@ -0,0 +1,14 @@
+
+public class SonarTest {
+		public String b;
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			public String a;
+			System.out.println(a);
+			System.out.println(sonar );
+			
+		}
+
+
+}
